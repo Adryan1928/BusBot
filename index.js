fa3858a2b1fb82edb8bdba0056c1799a6d832e44
brunohro <brunoh.rodrigues27@gmail.com>
2024-08-13 09:47:21 -0300

    Atualizando mensagens

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client = new Client({
 });
 
 client.on('ready', () => {
-    console.log('Client is ready!');
+    console.log('Cliente conectado!');
 });
 
 client.on('qr', qr => {
@@ -15,8 +15,15 @@ client.on('qr', qr => {
 
 client.on('message_create', message => {
     console.log(message.body)
-	if (message.body === '!ping') {
-		client.sendMessage(message.from, 'pong');
+	if (message.body != 'Bom dia! Eu sou o bot José, aqui para ajudar com perguntas acadêmicas e apoiar seus estudos. \nDigite: 01 para receber perguntas \nDigite: 02 para não receber perguntas.') {
+        if (message.body == '01') {
+            client.sendMessage(message.from,'Você escolheu 1')
+            return
+        } else if (message.body == 'Você escolheu 1') {
+            return
+        }
+		client.sendMessage(message.from, 'Bom dia, eu sou o bot José! \n Eu posso te enviar perguntas acadêmicas para você te apoiar nos seus estudos! \n Digite: 01; se você deseja receber \n Digite: 02; se não.');
+
 	}
 });
 
