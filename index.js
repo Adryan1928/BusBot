9279847c80f0ac7568180f4e41e2f3fe2dd94ae1
Bruno Henrique Oliveira <bh084996074995@gmail.com>
2024-08-14 12:05:18 -0300

    Com a mensagem "Mais" o bot enviar um link para o usuário

--- index.js
+++ index.js
@@ -21,8 +21,12 @@ client.on('message_create', message => {
     if (message.body === '01') {
         client.sendMessage(message.from, 'Você escolheu 1. Aqui estão suas perguntas: \n1. Pergunta A \n2. Pergunta B \n\nDigite "gabarito" para ver as respostas.');
     } else if (message.body.toLowerCase() === 'gabarito') {
-        client.sendMessage(message.from, 'Aqui está o gabarito: \n1. Resposta A \n2. Resposta B \n Digite: 02 para encerrar.');
-    } else if (message.body === '02') {
+        client.sendMessage(message.from, 'Aqui está o gabarito: \n1. Resposta A \n2. Resposta B \nDigite: "Mais" para ter acesso a mais questões. \nDigite: 02 para encerrar.');
+    } else if (message.body.toLowerCase() === 'mais') {
+        client.sendMessage(message.from, 'Quer mais perguntas como essas? Assine o "Descomplica" e tenha acesso a uma vasta coleção de questões e materiais de estudo! \nAproveite nosso CUPOM DE DESCONTO exclusivo: *BOTJOSE10* \nNão perca essa oportunidade de melhorar seus estudos. Assine agora!');
+        client.sendMessage(message.from, 'Estamos encerrando por aqui. Até a próxima!');
+    }
+     else if (message.body === '02') {
         client.sendMessage(message.from, 'Você escolheu encerrar. Até a próxima!');
     } else if (message.body !== welcomeMessage) {
         client.sendMessage(message.from, welcomeMessage);
