ef49f6c6bc0404f9e6c8d983e8188507bf4b13a6
Adryan1928 <adryanoliveira1928@gmail.com>
2024-09-03 20:00:39 -0300

    Fix STATIC_ROOT

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -114,7 +115,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
