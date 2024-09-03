249fae86f8df2fdbd4b57dc797f3770783658d0b
Adryan1928 <adryanoliveira1928@gmail.com>
2024-09-03 19:40:28 -0300

    Fix serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,12 +1,30 @@
 from rest_framework import serializers
-from .models import Cliente, Questao
+from .models import Cliente, Questao, Alternativa, Categoria
 
-class ClienteSerializer(serializers.ModelSerializer):
+class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Cliente
+        model = Categoria
         fields = '__all__'
 
+class AlternativaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Alternativa
+        fields = ['id', 'nome', 'correta']
+
 class QuestaoSerializer(serializers.ModelSerializer):
+    alternativas = serializers.SerializerMethodField()
+    categoria = CategoriaSerializer(many=True, read_only=True)
+
     class Meta:
         model = Questao
+        fields = ["id", "pergunta", "alternativas", "categoria"]
+    
+    def get_alternativas(self, instance):
+        alternativas = Alternativa.objects.filter(questao=instance)
+        return alternativas.distinct().values()
+
+class ClienteSerializer(serializers.ModelSerializer):
+    questoes_feitas = QuestaoSerializer(many=True, read_only=True)
+    class Meta:
+        model = Cliente
         fields = '__all__'
